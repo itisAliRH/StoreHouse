2180b3a82b2a9bdd33fbdcd871d6f6c22ca93540
Alireza Heidari <a.heydari@viratech.ir>
2019-05-10 20:06:40 +0430

    Add items,staffs and store houses menu

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from subprocess import call
 
 def connect_db():
     clear()
-    conn = sqlite3.connect('af.db')
+    connection = sqlite3.connect('af.db')
     print('Database connected successfully!')
     time.sleep(1)
+    return connection
 
 
 def clear():
@@ -61,15 +62,107 @@ def mdata_menu():
 
 
 def items_menu():
-    pass
+    clear()
+    print('*****Items Menu*****')
+    print('Please select:')
+    choice = input("""\nA: Add Item\nB: Delete Item\n\n0: Back\n\n""")
+    if choice == 'A' or choice == 'a':
+        add_item()
+    elif choice == 'B' or choice == 'b':
+        del_item()
+    elif choice == '0':
+        mdata_menu()
+    else:
+        retry()
+        items_menu()
+
+
+def add_item():
+    clear()
+    name = input('Name: ')
+    price = float(input('Price: '))
+    conn.execute('insert into Items (Name, Price) values (\'%s\',%.2f)' % (name, price))
+    clear()
+    print('%s with price %.2f added successfully!' % (name, price))
+    conn.commit()
+    time.sleep(1)
+    items_menu()
+
+
+def del_item():
+    items_menu()
 
 
 def staffs_menu():
-    pass
+    clear()
+    print('*****Staffs Menu*****')
+    print('Please select:')
+    choice = input("""\nA: Add Staff\nB: Delete Staff\n\n0: Back\n\n""")
+    if choice == 'A' or choice == 'a':
+        add_staff()
+    elif choice == 'B' or choice == 'b':
+        del_staff()
+    elif choice == '0':
+        mdata_menu()
+    else:
+        retry()
+        staffs_menu()
+
+
+def add_staff():
+    clear()
+    name = input('Name: ')
+    conn.execute('insert into Staffs (Name) values (\'%s\')' % name)
+    clear()
+    print('%s added successfully!' % name)
+    conn.commit()
+    time.sleep(1)
+    staffs_menu()
+
+
+def del_staff()
+    staffs_menu()
 
 
 def house_menu():
-    pass
+    clear()
+    print('*****Store House Menu*****')
+    print('Please select:')
+    choice = input("""\nA: Add Store House\nB: Delete Store House\n\n0: Back\n\n""")
+    if choice == 'A' or choice == 'a':
+        add_house()
+    elif choice == 'B' or choice == 'b':
+        del_house()
+    elif choice == '0':
+        mdata_menu()
+    else:
+        retry()
+        house_menu()
+
+
+def add_house():
+    clear()
+    city = ''
+    while len(city) <= 0:
+        clear()
+        city = input('City: ')
+    clear()
+    mid = ''
+    while mid == '':
+        clear()
+        for stf in conn.execute('select * from Staffs'):
+            print(stf, end='\t')
+        mid = input('\nManagement ID: ')
+    mid = int(mid)
+    conn.execute('insert into StoreHouses (City, MID) values (\'%s\',%d)' % (city, mid))
+    print('Store House in %s with Management ID %d added successfully!' % (city, mid))
+    conn.commit()
+    time.sleep(1)
+    house_menu()
+
+
+def del_house():
+    house_menu()
 
 
 def stock_menu():
@@ -90,5 +183,5 @@ def retry():
     time.sleep(1)
 
 
-connect_db()
+conn = connect_db()
 main_menu()
